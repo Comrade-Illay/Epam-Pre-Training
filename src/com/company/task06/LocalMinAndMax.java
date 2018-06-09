1eb5382dc1ef9a26d14a4ebc0b68cee7da9c08ef
Comrade-Illay <39280460+Comrade-Illay@users.noreply.github.com>
2018-06-09 22:49:53 +0300

    Add task 6

---
+++ src/com/company/task06/LocalMinAndMax.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class LocalMinAndMax {
+
+    public static double[] findLocalMin(double[][] matrix) {
+
+        for (int i = 1; i < matrix.length - 1; i++) {
+            for (int j = 1; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] < matrix[i - 1][j] && matrix[i][j] < matrix[i][j + 1] && matrix[i][j] < matrix[i + 1][j]
+                        && matrix[i][j] < matrix[i][j + 1]) {
+                    return new double[]{i, j};
+                }
+            }
+        }
+
+        return new double[]{-1, -1};
+    }
+
+    public static double[] findLocalMax(double[][] matrix) {
+
+        for (int i = 1; i < matrix.length - 1; i++) {
+            for (int j = 1; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] > matrix[i - 1][j] && matrix[i][j] > matrix[i][j + 1]
+                        && matrix[i][j] > matrix[i + 1][j]
+                        && matrix[i][j] > matrix[i][j + 1]) {
+                    return new double[]{i, j};
+                }
+            }
+        }
+
+        return new double[]{-1, -1};
+    }
+}
